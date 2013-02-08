de3b3bcb723b0a38bf096abc0c14fd9138c821cb
Scott Murray <scott.murray@eegeo.com>
2013-02-08 14:16:40 +0000

    update with build script for command line build

---
+++ build.for.simulator.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+sh "update.platform.sh"
+
+projectpath=$(pwd)/./
+targetName="ExampleApp"
+
+(cd $projectpath && xcodebuild -target $targetName -arch "i386" -sdk "iphonesimulator")
+resultcode=$?
+
+echo
+if [ $resultcode = 0 ] ; then
+  echo "COMPILE XCODE PROJECT SUCCEEDED"
+else
+  echo "COMPILE XCODE PROJECT FAILED"
+fi
+echo
+
+exit $resultcode
+
+exit 0
