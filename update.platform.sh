1f21bfbca8cfbcdb479e30e8d0c7e1eb771809c7
Scott Murray <scott.murray@eegeo.com>
2013-09-10 11:04:11 +0100

    Echo platform version on when unpacking platform libs.

--- update.platform.sh
+++ update.platform.sh
@@ -4,4 +4,6 @@ rm -rf ./Include
 curl http://s3.amazonaws.com/eegeo-static/sdk.package.tar.gz > ./sdk.package.tar.gz
 tar -zxvf ./sdk.package.tar.gz
 rm -f ./sdk.package.tar.gz
+value=`cat ./sdk.package/version.txt`
+echo "Platform version --> $value"
 mv ./sdk.package/ ./Include
