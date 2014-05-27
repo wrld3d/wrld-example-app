6660d206dc5314fb9503cefa548a711627c61e1c
Scott Murray <scott.murray@eegeo.com>
2014-05-27 09:21:03 +0100

    Get Android.mk working for windows ndk build for new sample layout.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -120,18 +120,26 @@ os_name:=$(shell uname -s)
 ifeq ($(os_name),Darwin)
 	platformincludes := $(shell find ./libs/platform -type d ! -path "*/OSX/*" ! -path "*/iOS/*")
 	LOCAL_C_INCLUDES := $(platformincludes:$(LOCAL_PATH)/%=%)
+
+	exampleincludes := $(shell find ./libs/../../src -type d)
+	LOCAL_C_INCLUDES += $(exampleincludes:$(LOCAL_PATH)/%=%)
+	
+	java_exampleincludes := $(shell find ./jni/Examples -type d)
+	LOCAL_C_INCLUDES += $(java_exampleincludes:$(LOCAL_PATH)/%=%)
 else
 	# assume windows if not specified for now (due to no uname)
 	platformincludes := $(shell dir .\libs\platform /ad-h /s /b)
 	LOCAL_C_INCLUDES := $(platformincludes:$(LOCAL_PATH)/%=%)
 	LOCAL_C_INCLUDES += ./libs/platform 
+
+	exampleincludes := $(shell dir .\libs\..\..\src /ad-h /s /b)
+	LOCAL_C_INCLUDES += $(exampleincludes:$(LOCAL_PATH)/%=%)
+	LOCAL_C_INCLUDES += .\libs\..\..\src
+	
+	java_exampleincludes := $(shell dir .\jni\Examples /ad-h /s /b)
+	LOCAL_C_INCLUDES += $(java_exampleincludes:$(LOCAL_PATH)/%=%)
+	LOCAL_C_INCLUDES += .\jni\Examples
 endif 
-
-exampleincludes := $(shell find ./libs/../../src -type d)
-LOCAL_C_INCLUDES += $(exampleincludes:$(LOCAL_PATH)/%=%)
-
-java_exampleincludes := $(shell find ./jni/Examples -type d)
-LOCAL_C_INCLUDES += $(java_exampleincludes:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES += ./jni/AppMessages
 LOCAL_C_INCLUDES += ./jni/AppMessages/AppToJava
