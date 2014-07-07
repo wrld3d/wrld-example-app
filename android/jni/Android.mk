3e0598ee6c4ae93ebfec1943a236afcfe906d9d0
Scott Murray <scott.murray@eegeo.com>
2014-07-07 16:44:45 +0100

    Link against httpxx binary in SDK distribution.

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -1,26 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := httpxx-lib
-
-LOCAL_CFLAGS += -march=armv6 -marm -mfloat-abi=softfp -mfpu=vfp -fexceptions
-
-cppfiles := $(shell cd jni; find ./../libs/httpxx/libs/http-parser/ ./../libs/httpxx/code/ -type f  -iname "*.c*")
-LOCAL_SRC_FILES := $(cppfiles:$(LOCAL_PATH)/%=%)
-
-LOCAL_C_INCLUDES := ./libs/httpxx/libs/http-parser
-include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := native-activity-lib
 LOCAL_SRC_FILES := ./../libs/libnative-activity-lib.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
+
 LOCAL_MODULE := native-activity
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -L./libs/ -lpng -lz -lm -L./libs/ -lcrypto -L./libs/ -lssl -L./libs/ -lcurl -L./libs/ -lcares -L./libs/ -lsimd -L./libs/ -lmyjpeg
+LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv2 -L./libs/ -lpng -lz -lm -L./libs/ -lcrypto -L./libs/ -lssl -L./libs/ -lcurl -L./libs/ -lcares -L./libs/ -lsimd -L./libs/ -lmyjpeg -L./libs/ -lhttpxx-lib
 LOCAL_STATIC_LIBRARIES := android_native_app_glue native-activity-lib 
-LOCAL_STATIC_LIBRARIES += httpxx-lib
 
 ifdef COMPILE_CPP_11
   LOCAL_LDLIBS += -fuse-ld=bfd
