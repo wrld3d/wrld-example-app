d0fec447fa9b9b1b2026c0699838ead988375fb8
Salvador Lopez <salvador.lopez@eegeo.com>
2014-11-25 14:23:49 +0000

    Review comment: Difficult to drag at altitude. Buddy: Ian H

--- src/MyPinCreation/PoiRing/PoiRingTouchController.cpp
+++ src/MyPinCreation/PoiRing/PoiRingTouchController.cpp
@@ -5,6 +5,7 @@
 #include "RenderCamera.h"
 #include "IMyPinCreationModel.h"
 #include "GlobeCameraController.h"
+#include "EarthConstants.h"
 
 namespace ExampleApp
 {
@@ -42,7 +43,9 @@ namespace ExampleApp
                 if (rayPick)
                 {
                     const Eegeo::dv3& currentPosition = m_myPinCreationModel.GetPosition();
-                    const float touchRadius = 170.f;
+                    float cameraAltitude = rayOrigin.Length() - Eegeo::Space::EarthConstants::Radius;
+                    const float touchRadius = 100.f + cameraAltitude/20;
+                    
                     
                     if ((rayIntersectionPoint - currentPosition).Length() < touchRadius)
                     {
