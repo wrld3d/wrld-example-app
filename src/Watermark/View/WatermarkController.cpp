83b4fbcd497a45b6ee4d4f8ca98a40483f6f1532
Malcolm Brown <DragonXVIFB&GMail.com>
2015-04-03 15:49:34 +0100

    Intro screen now requests watermark hide offscreen so it doesn't overlap on phone screens. Returns when dismissed. Buddy: Ian

--- src/Watermark/View/WatermarkController.cpp
+++ src/Watermark/View/WatermarkController.cpp
@@ -18,6 +18,18 @@ namespace ExampleApp
                 ScreenControl::View::Apply(m_viewModel, m_view);
             }
             
+            void WatermarkController::OnHandleSetVisibility(const SetWatermarkVisibilityMessage& message)
+            {
+                if(message.GetShouldSetVisible())
+                {
+                    m_viewModel.AddToScreen();
+                }
+                else
+                {
+                    m_viewModel.RemoveFromScreen();
+                }
+            }
+            
             WatermarkController::WatermarkController(
                 IWatermarkViewModel& viewModel,
                 IWatermarkView& view,
@@ -30,13 +42,16 @@ namespace ExampleApp
                 , m_metricsService(metricsService)
                 , m_selectedCallback(this, &WatermarkController::OnSelected)
                 , m_viewStateCallback(this, &WatermarkController::OnViewStateChangeScreenControl)
+                , m_setVisibilityHandler(this, &WatermarkController::OnHandleSetVisibility)
             {
                 m_view.InsertSelectedCallback(m_selectedCallback);
                 m_viewModel.InsertOnScreenStateChangedCallback(m_viewStateCallback);
+                m_messageBus.SubscribeUi(m_setVisibilityHandler);
             }
 
             WatermarkController::~WatermarkController()
             {
+                m_messageBus.UnsubscribeUi(m_setVisibilityHandler);
                 m_viewModel.RemoveOnScreenStateChangedCallback(m_viewStateCallback);
                 m_view.RemoveSelectedCallback(m_selectedCallback);
             }
--- src/Watermark/View/WatermarkController.h
+++ src/Watermark/View/WatermarkController.h
@@ -9,6 +9,7 @@
 #include "ApplyScreenControl.h"
 #include "MyPinCreationStateChangedMessage.h"
 #include "IMetricsService.h"
+#include "SetWatermarkVisibilityMessage.h"
 
 namespace ExampleApp
 {
@@ -25,11 +26,14 @@ namespace ExampleApp
 
                 Eegeo::Helpers::TCallback0<WatermarkController> m_selectedCallback;
                 Eegeo::Helpers::TCallback2<WatermarkController, ScreenControl::View::IScreenControlViewModel&, float> m_viewStateCallback;
+                Eegeo::Helpers::TCallback1<WatermarkController, const SetWatermarkVisibilityMessage&> m_setVisibilityHandler;
                 Metrics::IMetricsService& m_metricsService;
 
                 void OnSelected();
 
                 void OnViewStateChangeScreenControl(ScreenControl::View::IScreenControlViewModel& viewModel, float& state);
+                
+                void OnHandleSetVisibility(const SetWatermarkVisibilityMessage& message);
 
             public:
                 WatermarkController(
