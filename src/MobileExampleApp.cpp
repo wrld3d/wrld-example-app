53a4c62796946cbb4451188315961ad0485da3ae
Ian Reynolds <ian.reynolds@eegeo.com>
2015-08-18 10:05:33 +0100

    Fixed alpha on black loading background colour Buddy: Malcolm

--- src/MobileExampleApp.cpp
+++ src/MobileExampleApp.cpp
@@ -66,7 +66,7 @@ namespace ExampleApp
         {
             Eegeo::Rendering::LoadingScreenConfig loadingScreenConfig;
             loadingScreenConfig.layout = Eegeo::Rendering::LoadingScreenLayout::Centred;
-            loadingScreenConfig.backgroundColor = Eegeo::v4::Zero();
+            loadingScreenConfig.backgroundColor = Eegeo::v4(0.0f, 0.0f, 0.0f, 1.0f);
             loadingScreenConfig.loadingBarColor = Eegeo::v4::One();
             loadingScreenConfig.loadingBarBackgroundColor = Eegeo::v4(0.5f, 0.5f, 0.5f, 1.0f);
             loadingScreenConfig.fadeOutDurationSeconds = 1.5f;
