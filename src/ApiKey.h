e117c0cf82c66d3c07728ecba83e2a044004757f
Tim Jenks <tim.jenks@eegeo.com>
2015-09-24 17:00:27 +0100

    Fix up cpp11 project file.

--- src/ApiKey.h
+++ src/ApiKey.h
@@ -7,13 +7,13 @@
 namespace ExampleApp
 {
     //! REQUIRED: You must obtain an API key for the eeGeo SDK from https://www.eegeo.com/developers/
-    static const std::string ApiKey = "d6811a033724b916ae875aad3da4d39d";
+    static const std::string ApiKey = "OBTAIN API_KEY FROM https://www.eegeo.com/developers/ AND INSERT IT HERE";
     
     //! Optional: If 'useYelp' is true in AppHost you may wish to obtain a Yelp API key from https://www.yelp.com/developers for POI search provision
-    static const std::string YelpConsumerKey = "5XCRjDihxFzymofV7c026A";
-    static const std::string YelpConsumerSecret = "CypX2XlLek_a_PucVQabUotTM0c";
-    static const std::string YelpOAuthToken = "P7dxJs2lhwwwPgy9jSDyY7AGm78G8eoR";
-    static const std::string YelpOAuthTokenSecret = "NfNAVynMkDUAylvV4LKNl6EWhDY";
+    static const std::string YelpConsumerKey = "OBTAIN YELP CONSUMER KEY AND INSERT IT HERE";
+    static const std::string YelpConsumerSecret = "OBTAIN YELP CONSUMER SECRET AND INSERT IT HERE";
+    static const std::string YelpOAuthToken = "OBTAIN YELP OAUTH TOKEN AND INSERT IT HERE";
+    static const std::string YelpOAuthTokenSecret = "OBTAIN YELP OAUTH TOKEN SECRET AND INSERT IT HERE";
     
     //! Optional: You may wish to obtain a geonames account key from http://www.geonames.org/export/web-services.html for address/place search provision
     static const std::string GeoNamesUserName = "Eegeo_Cisco";
