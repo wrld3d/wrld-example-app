b653dab2486a3fac5bdb3f8a8d48e02e765ce059
Ian Reynolds <ian.reynolds@eegeo.com>
2015-10-06 12:02:15 +0100

    Fixes MPLY-5905: Search results not sorted correctly Added additional Swallow sort functionality: 	People sorted by surname first 	Meeting rooms sorted by availability first (added some constants for availability priority) 	Everything else sorted alphabetically by Title
    Buddy: Mark

--- src/SearchResultMenu/View/Swallow/SwallowSearchResultMenuOrder.cpp
+++ src/SearchResultMenu/View/Swallow/SwallowSearchResultMenuOrder.cpp
@@ -4,6 +4,8 @@
 
 #include "SearchResultModel.h"
 #include "SearchVendorNames.h"
+#include "SwallowSearchConstants.h"
+#include "SwallowSearchParser.h"
 
 namespace ExampleApp
 {
@@ -43,6 +45,70 @@ namespace ExampleApp
                     return Search::SwallowVendorPriority_MAX;
                 }
                 
+                int GetMeetingRoomAvailabilityPriority(const std::string& availability)
+                {
+                    if(availability == Search::Swallow::SearchConstants::MEETING_ROOM_AVAILABLE)
+                    {
+                        return Search::Swallow::SearchConstants::MEETING_ROOM_AVAILABLE_PRIORITY;
+                    }
+                    
+                    if(availability == Search::Swallow::SearchConstants::MEETING_ROOM_AVAILABLE_SOON)
+                    {
+                        return Search::Swallow::SearchConstants::MEETING_ROOM_AVAILABLE_SOON_PRIORITY;
+                    }
+                    
+                    if(availability == Search::Swallow::SearchConstants::MEETING_ROOM_OCCUPIED)
+                    {
+                        return Search::Swallow::SearchConstants::MEETING_ROOM_OCCUPIED_PRIORITY;
+                    }
+                    
+                    return Search::Swallow::SearchConstants::MEETING_ROOM_MAX_PRIORITY;
+                }
+                
+                std::string GetSurname(const std::string& name)
+                {
+                    size_t lastSpacePos = name.find_last_of(" ");
+                    
+                    if(lastSpacePos == std::string::npos || lastSpacePos == name.size() - 1)
+                    {
+                        return name;
+                    }
+                    
+                    return name.substr(lastSpacePos + 1);
+                }
+                
+                bool SwallowSubCategorySort(int priority, const Search::SdkModel::SearchResultModel& a, const Search::SdkModel::SearchResultModel& b)
+                {
+                    if(priority == Search::SwallowPeopleVendorPriority)
+                    {
+                        // presumably at some point we will store the surname separately, this function should suffice in the meantime
+                        std::string aSurname = GetSurname(a.GetTitle());
+                        std::string bSurname = GetSurname(b.GetTitle());
+                        
+                        if(aSurname != bSurname)
+                        {
+                            return aSurname < bSurname;
+                        }
+                    }
+                    
+                    if(priority == Search::SwallowMeetingRoomsVendorPriority)
+                    {
+                        const Search::Swallow::SdkModel::SwallowMeetingRoomResultModel& aMeetingRoomResult = Search::Swallow::SdkModel::SearchParser::TransformToSwallowMeetingRoomResult(a);
+                        
+                        const Search::Swallow::SdkModel::SwallowMeetingRoomResultModel& bMeetingRoomResult = Search::Swallow::SdkModel::SearchParser::TransformToSwallowMeetingRoomResult(b);
+                        
+                        int aPriority = GetMeetingRoomAvailabilityPriority(aMeetingRoomResult.GetAvailability());
+                        int bPriority = GetMeetingRoomAvailabilityPriority(bMeetingRoomResult.GetAvailability());
+                        
+                        if(aPriority != bPriority)
+                        {
+                            return aPriority < bPriority;
+                        }
+                    }
+                    
+                    return a.GetTitle() < b.GetTitle();
+                }
+                
                 bool SwallowSearchResultMenuOrder::operator() (const Search::SdkModel::SearchResultModel& a, const Search::SdkModel::SearchResultModel& b)
                 {
                     bool aIsSwallowResult = a.GetVendor().find(Search::SwallowVendorName) != std::string::npos;
@@ -63,7 +129,14 @@ namespace ExampleApp
                         int aPriority = GetSwallowResultPriority(a);
                         int bPriority = GetSwallowResultPriority(b);
                         
-                        return aPriority < bPriority;
+                        if(aPriority != bPriority)
+                        {
+                            return aPriority < bPriority;
+                        }
+                        else
+                        {
+                            return SwallowSubCategorySort(aPriority, a, b);
+                        }
                     }
                     
                     //GeoNames results on top
