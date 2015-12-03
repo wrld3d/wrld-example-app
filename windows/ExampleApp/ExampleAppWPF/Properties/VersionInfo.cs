c40262c8f655d1a892ccd96c2b0eaee4922ffd1d
Jonty Dawson <jonty.dawson@eegeo.com>
2015-12-03 10:47:57 +0000

    Windows app: parameterise assembly info ProductName, FileVersion, InformationalFileVersion. Poke with optional args in build script Buddy: Ian R

---
+++ windows/ExampleApp/ExampleAppWPF/Properties/VersionInfo.cs
@@ -0,0 +1,9 @@
+﻿// Copyright eeGeo Ltd (2012-2015), All Rights Reserved
+internal struct VersionInfo
+{
+    public const string Company = @"eeGeo Ltd";
+    public const string Copyright = @"Copyright (c) 2015 eeGeo Ltd. All rights reserved.";
+    public const string ProductName = @"eeGeo Example";
+    public const string FileVersion = @"0.1.0.0";
+    public const string InformationalFileVersion = @"0.1.0.0";
+}
--- windows/build.sh
+++ windows/build.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-usage() { echo "Usage: $0 -p windows -k <APIKEY>"; echo "  -p -> platform, windows (required)"; echo "  -k -> the Eegeo API key to use for the app"; 1>&2; exit 1; }
+usage()
+{ 
+	echo "Usage: $0 -p windows -k <APIKEY> [-n <PRODUCT_NAME> -v <FILE_VERSION> -i <INFORMATIONAL_VERSION>]"; 
+	echo "  -p -> platform, windows (required)"; 
+	echo "  -k -> the Eegeo API key to use for the app (required)"; 
+	echo "  -n -> the product name of the app"; 
+	echo "  -v -> file version of executable"; 
+	echo "  -i -> informational version of executable"; 
+	1>&2; 
+	exit 1; 
+}
 
 projectPath=$(pwd)/./
 
-while getopts "p:k:" o; do
+while getopts "p:k:n:v:i:" o; do
     case "${o}" in
         p)
             p=${OPTARG}
@@ -15,7 +25,16 @@ while getopts "p:k:" o; do
     	k)
             k=${OPTARG}
             ;;
-        *)
+    	n)
+            productName=${OPTARG}
+            ;;
+    	v)
+            fileVersion=${OPTARG}
+            ;;
+    	i)
+            informationalVersion=${OPTARG}
+            ;;
+		*)
             usage
             ;;
     esac
@@ -31,6 +50,22 @@ if [ -z "${k}" ]; then
 fi
 
 sed -i "s/std::string\s*ApiKey\s*=\s*\"[^\"]*\"/std::string ApiKey = \"$k\"/g" src/ApiKey.h
+
+
+
+if [ ! -z "${productName}" ]; then
+    sed -i "s/string\s*ProductName\s*=\s*@\"[^\"]*\"/string ProductName = @\"$productName\"/g" windows/ExampleApp/ExampleAppWPF/Properties/VersionInfo.cs
+	sed -i "s/\s*Title\s*=\s*\"[^\"]*\"/        Title=\"$productName\"/g" windows/ExampleApp/ExampleAppWPF/MainWindow.xaml
+	
+fi
+
+if [ ! -z "${fileVersion}" ]; then
+    sed -i "s/string\s*FileVersion\s*=\s*@\"[^\"]*\"/string FileVersion = @\"$fileVersion\"/g" windows/ExampleApp/ExampleAppWPF/Properties/VersionInfo.cs
+fi
+
+if [ ! -z "${informationalVersion}" ]; then
+    sed -i "s/string\s*InformationalFileVersion\s*=\s*@\"[^\"]*\"/string InformationalFileVersion = @\"$informationalVersion\"/g" windows/ExampleApp/ExampleAppWPF/Properties/VersionInfo.cs
+fi
 
 if [ ! -f windows/nuget.exe ]; then
 	curl -o windows/nuget.exe https://api.nuget.org/downloads/nuget.exe
