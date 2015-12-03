679497476cef36896d06ef8c36596960a1ec7e16
Ian Reynolds <ian.reynolds@kimbleoperations.com>
2015-12-03 18:19:35 +0000

    Added custom windows dialog box and replaced calls to MessageBox for watermark and POI / MyPin views.
    Buddy: Jonty

---
+++ windows/ExampleApp/ExampleAppWPF/DialogBox.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ExampleAppWPF
+{
+    public class DialogViewModel
+    {
+        public DialogViewModel(string title, string message, string acceptButton, string cancelButton)
+        {
+            TitleText = title;
+            MessageText = message;
+            AcceptButtonText = acceptButton;
+            CancelButtonText = cancelButton;
+        }
+
+        public string TitleText { get; set; }
+        public string MessageText { get; set; }
+        public string AcceptButtonText { get; set; }
+        public string CancelButtonText { get; set; }
+    }
+
+    public partial class DialogBox : Window
+    {
+        public DialogBox(string title, string message, string acceptButton, string cancelButton)
+        {
+            InitializeComponent();
+
+            DataContext = new DialogViewModel(title, message, acceptButton, cancelButton);
+        }
+
+        void OnAcceptButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
