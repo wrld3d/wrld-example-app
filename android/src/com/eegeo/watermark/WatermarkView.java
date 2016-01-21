d40b4bf7d61c2aa3c1277a2c2ee6a0ff7cb691bd
Malcolm Brown <DragonXVIFB&GMail.com>
2016-01-21 18:13:13 +0000

    Fixes for MPLY-6145, MPLY-6312, MPLY-6408. Watermark on android should now correctly show Micello logo without crashing & be positioned correctly. Buddy: Ian R

--- android/src/com/eegeo/watermark/WatermarkView.java
+++ android/src/com/eegeo/watermark/WatermarkView.java
@@ -5,6 +5,8 @@ package com.eegeo.watermark;
 import com.eegeo.entrypointinfrastructure.MainActivity;
 import com.eegeo.mobileexampleapp.R;
 
+import android.animation.Animator;
+import android.animation.Animator.AnimatorListener;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -17,6 +19,7 @@ public class WatermarkView implements View.OnClickListener
  private MainActivity m_activity = null;
  private long m_nativeCallerPointer;
  private View m_view = null;
+ private View m_gradientView = null;
  private static AlertDialog m_options = null;
  
  private String m_imageAssetUrl;
@@ -24,11 +27,13 @@ public class WatermarkView implements View.OnClickListener
  private String m_popupBody;
  private String m_webUrl;
  private boolean m_shouldShowShadow;
+ private boolean m_alignAlongBottom;
 
  private float m_yPosActive;
  private float m_yPosInactive;
 
  private final long m_stateChangeAnimationTimeMilliseconds = 200;
+ private AnimatorListener m_transitionOffListener;
 
  public WatermarkView(MainActivity activity, 
 		 			  long nativeCallerPointer, 
@@ -40,10 +45,44 @@ public class WatermarkView implements View.OnClickListener
  {
      m_activity = activity;
      m_nativeCallerPointer = nativeCallerPointer;
+     m_transitionOffListener = new AnimatorListener() 
+     {
+		@Override
+		public void onAnimationCancel(Animator animation) 
+		{
+			animation.removeAllListeners();
+		}
+
+		@Override
+		public void onAnimationEnd(Animator animator) 
+		{
+			
+			String uri = "drawable/" + m_imageAssetUrl;
+		    int imageResource = m_activity.getResources().getIdentifier(uri, null, m_activity.getPackageName());
+			m_view.setBackgroundResource(imageResource);
+			 
+			refreshPositions();
+			m_view.setY(m_yPosInactive);
+			animateToActive();
+			
+			float gradientAlpha = m_shouldShowShadow ? 1.0f : 0.0f;
+			m_gradientView.animate().alpha(gradientAlpha).setDuration(m_stateChangeAnimationTimeMilliseconds);
+		}
+
+		@Override
+		public void onAnimationRepeat(Animator animation) 
+		{
+		}
+
+		@Override
+		public void onAnimationStart(Animator animation) 
+		{
+		}
+     };
 
      final RelativeLayout uiRoot = (RelativeLayout)m_activity.findViewById(R.id.ui_container);
+     m_gradientView = m_activity.getLayoutInflater().inflate(R.layout.watermark_background_layout, uiRoot, false);
      m_view = m_activity.getLayoutInflater().inflate(R.layout.watermark_layout, uiRoot, false);
-
      m_view.setOnClickListener(this);
      
      m_view.addOnLayoutChangeListener(new View.OnLayoutChangeListener() 
@@ -53,25 +92,7 @@ public class WatermarkView implements View.OnClickListener
 					int bottom, int oldLeft, int oldTop, int oldRight,
 					int oldBottom) 
 			{   
-		        final float screenHeight = uiRoot.getHeight();
-		        final float screenWidth = uiRoot.getWidth();
-		        final float viewHeight = m_view.getHeight();
-		        final float viewWidth = m_view.getWidth();
-		        
-		        if(m_activity.getResources().getBoolean(R.bool.isPhone))
-		        {
-		            m_yPosActive = m_activity.dipAsPx(20);
-		            m_yPosInactive = (-viewHeight);
-
-			        m_view.setX((screenWidth*0.5f) - (viewWidth*0.5f));
-		        }
-		        else
-		        {
-		            m_yPosActive = (screenHeight - viewHeight) - m_activity.dipAsPx(8);
-		            m_yPosInactive = (screenHeight + viewHeight);
-
-			        m_view.setX((screenWidth - viewWidth) - m_activity.dipAsPx(8));
-		        }
+				refreshPositions();
 
 		        m_view.setY(m_yPosInactive);
 		        m_view.removeOnLayoutChangeListener(this);
@@ -81,6 +102,9 @@ public class WatermarkView implements View.OnClickListener
      updateWatermarkData(imageAssetUrl, popupTitle, popupBody, webUrl, shouldShowShadow);
      
      m_view.setAlpha(0.8f);
+     m_gradientView.setAlpha(0.0f);
+     m_gradientView.setY(uiRoot.getHeight() - m_activity.dipAsPx(52.0f));
+     uiRoot.addView(m_gradientView);
      uiRoot.addView(m_view);
  }
 
@@ -94,7 +118,9 @@ public class WatermarkView implements View.OnClickListener
      
      final RelativeLayout uiRoot = (RelativeLayout)m_activity.findViewById(R.id.ui_container);
      uiRoot.removeView(m_view);
+     uiRoot.removeView(m_gradientView);
      m_view = null;
+     m_gradientView = null;
  }
 
  @Override
@@ -126,6 +152,7 @@ public class WatermarkView implements View.OnClickListener
  {
      m_view.animate()
      .y(yAsPx)
+     .setListener(null)
      .setDuration(m_stateChangeAnimationTimeMilliseconds);
  }
 
@@ -162,9 +189,11 @@ public class WatermarkView implements View.OnClickListener
  
  private void transitionToNewImage()
  {
-	 String uri = "drawable/" + m_imageAssetUrl;
-     int imageResource = m_activity.getResources().getIdentifier(uri, null, m_activity.getPackageName());
-	 m_view.setBackgroundResource(imageResource);
+	 m_view.clearAnimation();
+	 m_view.animate()
+	 .y(m_yPosInactive)
+	 .setDuration(m_stateChangeAnimationTimeMilliseconds)
+	 .setListener(m_transitionOffListener);
  }
  
  private DialogInterface.OnClickListener createClickListener(final boolean shouldPreload)
@@ -190,4 +219,42 @@ public class WatermarkView implements View.OnClickListener
 	 
      m_options = null;
  }
+ 
+ public void setAlignmentState(final boolean alignAlongBottom)
+ {
+	 m_alignAlongBottom = alignAlongBottom;
+ }
+
+    private void refreshPositions()
+    {
+        final RelativeLayout uiRoot = (RelativeLayout)m_activity.findViewById(R.id.ui_container);
+        
+        final float screenHeight = uiRoot.getHeight();
+        final float screenWidth = uiRoot.getWidth();
+        final float viewHeight = m_view.getHeight();
+        final float viewWidth = m_view.getWidth();
+        
+        if(m_activity.getResources().getBoolean(R.bool.isPhone))
+        {
+            if(m_alignAlongBottom)
+            {
+                m_yPosActive = (screenHeight - viewHeight) - m_activity.dipAsPx(8);
+                m_yPosInactive = (screenHeight + viewHeight);
+            }
+            else
+            {
+                m_yPosActive = m_activity.dipAsPx(20);
+                m_yPosInactive = (-viewHeight);
+            }
+            
+            m_view.setX((screenWidth*0.5f) - (viewWidth*0.5f));
+        }
+        else
+        {
+            m_yPosActive = (screenHeight - viewHeight) - m_activity.dipAsPx(8);
+            m_yPosInactive = (screenHeight + viewHeight);
+
+            m_view.setX((screenWidth - viewWidth) - m_activity.dipAsPx(8));
+        }
+    }
 }
