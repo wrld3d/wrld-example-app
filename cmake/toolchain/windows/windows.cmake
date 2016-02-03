ec1ac3a8696bf56550159f5304660e39a329f708
Ian Hutchinson <ian.hutchinson@eegeo.com>
2016-02-03 10:43:03 +0000

    Adding a dot net reference to WindowsBase to the C++ ExampleApp lib. Wrapping this in a new macro added to Windows toolchain. Buddy: Malc B

--- cmake/toolchain/windows/windows.cmake
+++ cmake/toolchain/windows/windows.cmake
@@ -17,3 +17,7 @@ macro (remove_visual_studio_compiler_flag_debug FLAG_TO_REMOVE)
 	string(REPLACE ${FLAG_TO_REMOVE} "" TEMP_CMAKE_CXX_FLAGS_DEBUG ${CMAKE_CXX_FLAGS_DEBUG})
 	set(CMAKE_CXX_FLAGS_DEBUG ${TEMP_CMAKE_CXX_FLAGS_DEBUG})
 endmacro (remove_visual_studio_compiler_flag_debug)
+
+macro (add_dotnet_reference TARGET REFERENCE_TO_ADD)
+	set_target_properties(${TARGET} PROPERTIES VS_DOTNET_REFERENCES ${REFERENCE_TO_ADD})
+endmacro(add_dotnet_reference)
