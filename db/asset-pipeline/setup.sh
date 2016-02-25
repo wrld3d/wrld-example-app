4625f4e17d0adb3b3c6ae26b35a58c985123bb65
Tim Jenks <tim.jenks@eegeo.com>
2016-02-25 15:00:43 +0000

    Attempted fix for POI db pipeline on Windows

--- db/asset-pipeline/setup.sh
+++ db/asset-pipeline/setup.sh
@@ -3,8 +3,4 @@
 curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
 easy_install pip==1.4.1
 pip install --user --upgrade -r pip_requirements
-easy_install lib/deps/PIL-1.1.7.win32-py2.7.exe
-
-
-
-
+easy_install lib/deps/Pillow-3.1.0.win32-py2.6.exe
