4d30cbc6c3986c7d913d54d7a72aa5208f31283c
Tim Jenks <tim.jenks@eegeo.com>
2016-03-02 15:29:41 +0000

    Fix up swallow result ordering client-side, to group by category priority

--- src/SearchResultSection/View/SearchResultSectionOrder.cpp
+++ src/SearchResultSection/View/SearchResultSectionOrder.cpp
@@ -4,6 +4,7 @@
 
 #include "SearchResultModel.h"
 #include "SearchVendorNames.h"
+#include "SwallowSearchConstants.h"
 
 namespace ExampleApp
 {
@@ -11,12 +12,45 @@ namespace ExampleApp
     {
         namespace View
         {
+            namespace
+            {                
+                int GetSwallowResultPriority(const Search::SdkModel::SearchResultModel& result)
+                {
+                    if(result.GetCategory() == Search::Swallow::SearchConstants::PERSON_CATEGORY_NAME)
+                    {
+                        return Search::SwallowPeopleVendorPriority;
+                    }
+                    
+                    if(result.GetCategory() == Search::Swallow::SearchConstants::MEETING_ROOM_CATEGORY_NAME)
+                    {
+                        return Search::SwallowMeetingRoomsVendorPriority;
+                    }
+                    
+                    if(result.GetCategory() == Search::Swallow::SearchConstants::WORKING_GROUP_CATEGORY_NAME)
+                    {
+                        return Search::SwallowWorkingGroupsVendorPriority;
+                    }
+                    
+                    if(result.GetCategory() == Search::Swallow::SearchConstants::FACILITY_CATEGORY_NAME)
+                    {
+                        return Search::SwallowFacilitiesVendorPriority;
+                    }
+                    
+                    if(result.GetCategory() == Search::Swallow::SearchConstants::OFFICE_CATEGORY_NAME)
+                    {
+                        return Search::SwallowOfficesVendorPriority;
+                    }
+                    
+                    return Search::SwallowVendorPriority_MAX;
+                }
+            }
+            
             bool SearchResultSectionOrder::operator() (const Search::SdkModel::SearchResultModel& a, const Search::SdkModel::SearchResultModel& b)
             {
                 //Order eeGeo results on top
                 if(a.GetVendor() != b.GetVendor() && a.GetVendor() == Search::EegeoVendorName)
                 {
-                        return true;
+                    return true;
                 }
                 //Order GeoNames results as they came in
                 if(a.GetVendor() == Search::GeoNamesVendorName || b.GetVendor() == Search::GeoNamesVendorName)
@@ -26,7 +60,16 @@ namespace ExampleApp
                 //Order eeGeo results as they came in
                 if(a.GetVendor() == Search::EegeoVendorName || b.GetVendor() == Search::EegeoVendorName)
                 {
-                    return false;
+                    if (a.GetCategory() == b.GetCategory())
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        int aPriority = GetSwallowResultPriority(a);
+                        int bPriority = GetSwallowResultPriority(b);
+                        return aPriority < bPriority;
+                    }
                 }
                 return a < b;
             }
