4c2ffa4410b7a1407e5b9092e2c11a70b91f824a
jonty-dawson-eegeo <jonty.dawson@eegeo.com>
2016-03-16 12:50:32 +0000

    fix MPLY-6504 'Opening the Search results menu whilst the camera is moving can show the results list refreshing multiple times' - adds extra speed threshold to SearchRefreshService, and takes viewpoint distance into account. Buddy: John

--- src/MobileExampleApp.cpp
+++ src/MobileExampleApp.cpp
@@ -983,7 +983,7 @@ namespace ExampleApp
 
         eegeoWorld.Update(updateParameters);
 
-        m_pSearchModule->GetSearchRefreshService().TryRefreshSearch(dt, ecefInterestPoint);
+        m_pSearchModule->GetSearchRefreshService().TryRefreshSearch(dt, ecefInterestPoint, cameraState.LocationEcef());
 
         m_pPinsModule->GetController().Update(dt, renderCamera);
         
