4bdf13e1f3c0d3aee2fb02884ef0a9f3145ccc78
Vimmy <me@vimarshraina.com>
2016-03-24 15:40:57 +0000

    Only update image when data is available, added checks to ensure data integrity. Buddy: Tim

--- windows/src/SearchResultPoi/View/SearchResultPoiView.cpp
+++ windows/src/SearchResultPoi/View/SearchResultPoiView.cpp
@@ -79,6 +79,11 @@ namespace ExampleApp
 
             void SearchResultPoiView::UpdateImage(const std::string& url, bool hasImage, const std::vector<unsigned char>* pImageBytes)
             {
+                if (!hasImage || pImageBytes == NULL)
+                {
+                    return;
+                }
+                
                 array<System::Byte>^ imageDataArray = gcnew array<System::Byte>(static_cast<int>(pImageBytes->size()));
 
                 for (size_t i = 0; i < pImageBytes->size(); ++i)
