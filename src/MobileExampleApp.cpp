e787a5b0806cf711809ad8caa520cccc0bca4171
Vimmy <me@vimarshraina.com>
2016-03-25 15:14:29 +0000

    Fix for MPLY-6756. Change alpha for desk highlighting. Buddy: Oliver

--- src/MobileExampleApp.cpp
+++ src/MobileExampleApp.cpp
@@ -381,7 +381,7 @@ namespace ExampleApp
                                                                                                                     m_pSearchModule->GetSearchResultRepository(),
                                                                                                                     mapModule.GetInteriorsModelModule().GetInteriorsInstanceRepository(),
                                                                                                                     m_messageBus,
-                                                                                                                    Eegeo::v4(0.0f, 1.0f, 0.0f, 0.6f),
+                                                                                                                    Eegeo::v4(0.0f, 1.0f, 0.0f, 0.8f),
                                                                                                                     mapModule.GetInteriorsModelModule().GetInteriorsCellResourceObserver());
         
         
