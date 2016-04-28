d34928e8fa2cdd13466b4a958470d98e1c820c68
Malcolm Brown <DragonXVIFB&GMail.com>
2016-04-28 16:06:05 +0100

    Fix for MPLY-6921 with inconsistent close button states on iOS. Buddy: Mark

--- ios/ios_src/SearchResultPoi/View/SwallowWorkingGroupSearchResultPoiView.mm
+++ ios/ios_src/SearchResultPoi/View/SwallowWorkingGroupSearchResultPoiView.mm
@@ -52,7 +52,7 @@ namespace
         [self.pControlContainer addSubview: self.pCloseButtonContainer];
         
         self.pCloseButton = [[[UIButton alloc] initWithFrame:CGRectMake(0, 0, 0, 0)] autorelease];
-        [self.pCloseButton setDefaultStatesWithImageNames:@"button_close_off" :@"button_close_off"];
+        [self.pCloseButton setDefaultStatesWithImageNames:@"button_close_off" :@"button_close_on"];
         [self.pCloseButton addTarget:self action:@selector(handleClosedButtonSelected) forControlEvents:UIControlEventTouchUpInside];
         [self.pCloseButtonContainer addSubview: self.pCloseButton];
         
