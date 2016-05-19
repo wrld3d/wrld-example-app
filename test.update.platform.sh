a5c99ec0b29a6f571ea495e2bc818e1184607d8d
Ian Hutchinson <ian.hutchinson@eegeo.com>
2016-05-19 17:24:56 +0100

    Update platform for systest

---
+++ test.update.platform.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+usage() { echo "Usage: $0 -p android|ios|windows [-c]"; echo "  -p -> platform, ios, android or windows (required)"; echo "  -c -> cpp03 support"; 1>&2; exit 1; }
+
+baseUrl="http://s3.amazonaws.com/eegeo-static/"
+srcPackageName="INVALID"
+destPackageName="./sdk.package.tar.gz"
+includeDestination="INVALID"
+sdkDestination="INVALID"
+
+while getopts "p:c" o; do
+    case "${o}" in
+        p)
+            p=${OPTARG}
+            if [ "$p" != "ios" ]; then
+               if [ "$p" != "android" ]; then
+	          if [ "$p" != "windows" ]; then
+                     usage
+		  fi
+               fi
+            fi
+            ;;
+        c)
+            c="cpp03"
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${p}" ]; then
+    usage
+fi
+
+if [ "$p" == "ios" ]; then
+   srcPackageName="sdk.package.ios"
+   includeDestination="./ios/Include/eegeo"
+   sdkDestination="sdk.package"
+elif [ "$p" == "android" ]; then
+   srcPackageName="TEST_so.test.sdk.package.android.tar.gz"
+   includeDestination="./android/libs/eegeo"
+   sdkDestination="sdk.package.android"
+elif [ "$p" == "windows" ]; then
+   srcPackageName="sdk.package.windows"
+   includeDestination="./windows/libs/eegeo"
+   sdkDestination="sdk.package.windows"
+
+fi
+
+: << 'BLEH'
+if [[ ( "$c" == "cpp03" ) || ( "$p" == "windows" ) ]]; then
+   srcPackageName="$srcPackageName.tar.gz"
+else
+   srcPackageName="$srcPackageName.cpp11.tar.gz"
+fi
+
+BLEH
+
+echo "Updating $p platform..."
+rm -f ./$destPackageName
+rm -rf $includeDestination
+curl $baseUrl$srcPackageName > ./$destPackageName
+
+statuscode=$?
+if [ $statuscode -ne 0 ] ; then
+    echo "Failed to download sdk package ${baseUrl}${srcPackageName}" >&2
+    exit $statuscode
+fi    
+
+if [ ! -d `dirname "$includeDestination"` ]; then
+    mkdir -p `dirname "$includeDestination"`
+fi
+
+tar -zxvf $destPackageName
+
+if [ $? -ne 0 ] ; then
+    echo "Failed to unzip sdk package ${destPackageName}"
+    exit 1    
+fi
+
+rm -f ./$destPackageName
+platformVersion=`cat ./$sdkDestination/version.txt`
+echo "Platform version --> $platformVersion"
+echo mv ./$sdkDestination $includeDestination
+mv ./$sdkDestination $includeDestination
