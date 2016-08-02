926a66253616d1abe1120e65293e4765f050a78c
Vimmy <me@vimarshraina.com>
2016-08-02 15:19:25 +0100

    Source ez_setup installer from different location since the repo has moved. Buddy: Ian H

--- db/asset-pipeline/setup.sh
+++ db/asset-pipeline/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py | python
+curl https://bootstrap.pypa.io/ez_setup.py | python
 easy_install pip==1.4.1
 pip install --user --upgrade -r pip_requirements
 easy_install lib/deps/Pillow-3.1.1.win32-py2.7.exe
