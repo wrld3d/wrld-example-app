c1c9ba4aa71330a06dbde07171a35cdecdfca0a1
MichaelChanEeGeo <michael.chan@Michaels-iMac.local>
2016-08-29 16:47:06 +0100

    Fix for MPLY-7381 on android. Changed order of 'post' and no longer using a delay. Buddy: Paul

--- android/src/com/eegeo/searchmenu/SearchResultsScrollButtonTouchDownListener.java
+++ android/src/com/eegeo/searchmenu/SearchResultsScrollButtonTouchDownListener.java
@@ -14,7 +14,7 @@ public class SearchResultsScrollButtonTouchDownListener implements View.OnTouchL
 {
 	private ListView m_searchList = null;
     private final Handler m_handler = new Handler();
-    private static  long m_delay = 100L/120L;
+
     
 	public SearchResultsScrollButtonTouchDownListener(ListView view) {
 		m_searchList = view;
@@ -23,12 +23,12 @@ public class SearchResultsScrollButtonTouchDownListener implements View.OnTouchL
     final Runnable m_action = new Runnable() {
 		@SuppressLint("NewApi")
 		@Override public void run() {
-            m_handler.postDelayed(this, m_delay);
             if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT)
             {   
             	m_searchList.scrollListBy(SearchMenuResultsListAnimationConstants.SearchMenuResultsListScrollButtonSpeed);
             	m_searchList.setVerticalScrollBarEnabled(false);
             }
+            m_handler.post(this);
         }
     };
     
@@ -36,7 +36,7 @@ public class SearchResultsScrollButtonTouchDownListener implements View.OnTouchL
 	public boolean onTouch(View v, MotionEvent event) {
 		switch(event.getAction()) {
 			case MotionEvent.ACTION_DOWN:
-				m_handler.postDelayed(m_action, m_delay);
+				m_handler.post(m_action);
 	            break;
 	        case MotionEvent.ACTION_UP:
 	        	m_handler.removeCallbacks(m_action);
