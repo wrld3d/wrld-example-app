ab097bb9f88efff99a5edcd4722dbb357831b06a
Mark Simpson <mark.simpson@eegeo.com>
2016-09-06 15:31:02 +0100

    Fix missing #pragma once usage in headers
    We were missing a few of these, so cleaned 'em up.'
    
    Buddy: Malcolm B

--- ios/ios_src/ImageStore/ImageFetchOperation.h
+++ ios/ios_src/ImageStore/ImageFetchOperation.h
@@ -1,4 +1,5 @@
 // Copyright eeGeo Ltd (2012-2015), All Rights Reserved
+#pragma once
 
 @protocol ImageFetchOperationDelegate <NSObject>
 
--- ios/ios_src/Tours/View/TourWeb/TourWebView.h
+++ ios/ios_src/Tours/View/TourWeb/TourWebView.h
@@ -1,4 +1,5 @@
 // Copyright eeGeo Ltd (2012-2015), All Rights Reserved
+#pragma once
 
 #import <UIKit/UIKit.h>
 #include <string>
