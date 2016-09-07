e175327f95d228bd30a41f0319360cf23a6c22df
blairmeegeo <blair.mcsorley@kimbleoperations.com>
2016-09-07 17:40:29 +0100

    Updating RHS menu to match EEA RHS style, iOS. Buddy: Calum

--- src/SettingsMenu/SdkModel/SettingsMenuModule.cpp
+++ src/SettingsMenu/SdkModel/SettingsMenuModule.cpp
@@ -27,17 +27,17 @@ namespace ExampleApp
                                                                     identityProvider.GetNextIdentity(),
                                                                     reactionControllerModel);
                 
-                m_pAboutMenuModel = Eegeo_NEW(Menu::View::MenuModel)();
-                m_pAboutMenuOptionsModel = Eegeo_NEW(Menu::View::MenuOptionsModel)(*m_pAboutMenuModel);
-                m_pAboutMenuOptionsModel->AddItem("About", "About", "", "misc", Eegeo_NEW(View::AboutPageMenuOption)(*m_pViewModel,aboutPageViewModel));
-                
-                AddMenuSection("About", "misc", *m_pAboutMenuModel, false);
-                
                 m_pOptionsMenuModel = Eegeo_NEW(Menu::View::MenuModel)();
                 m_pOptionsMenuOptionsModel = Eegeo_NEW(Menu::View::MenuOptionsModel)(*m_pOptionsMenuModel);
                 m_pOptionsMenuOptionsModel->AddItem("Options", "Options", "", "options", Eegeo_NEW(View::OptionsMenuOption)(*m_pViewModel, optionsViewModel));
                 
                 AddMenuSection("Options", "options", *m_pOptionsMenuModel, false);
+                
+                m_pAboutMenuModel = Eegeo_NEW(Menu::View::MenuModel)();
+                m_pAboutMenuOptionsModel = Eegeo_NEW(Menu::View::MenuOptionsModel)(*m_pAboutMenuModel);
+                m_pAboutMenuOptionsModel->AddItem("About", "About", "", "misc", Eegeo_NEW(View::AboutPageMenuOption)(*m_pViewModel,aboutPageViewModel));
+                
+                AddMenuSection("About", "misc", *m_pAboutMenuModel, false);
             }
             
             SettingsMenuModule::~SettingsMenuModule()
