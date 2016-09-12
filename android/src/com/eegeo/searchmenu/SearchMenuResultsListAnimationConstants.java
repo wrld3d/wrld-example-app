300ac98416022f2028770b49b2f7a2e05cc6e7e9
blairmeegeo <blair.mcsorley@kimbleoperations.com>
2016-09-12 15:34:26 +0100

    Changing blinds animation to scale from the top rather than the centre, changing timing of blind animation parts, Android. Buddy: Michael

--- android/src/com/eegeo/searchmenu/SearchMenuResultsListAnimationConstants.java
+++ android/src/com/eegeo/searchmenu/SearchMenuResultsListAnimationConstants.java
@@ -4,4 +4,9 @@ public class SearchMenuResultsListAnimationConstants
 {
 	static public final long SearchMenuResultsListScrollButtonAnimationSpeedMilliseconds = 1000;
 	static public final int SearchMenuResultsListScrollButtonSpeed = 6;
+	
+	static public final int SearchMenuListTotalAnimationSpeedMilliseconds = 200;
+	
+	static public final int SearchMenuItemExpandAnimationDelayMilliseconds = 400;
+	static public final int SearchMenuItemScaleAnimationSpeedMilliseconds = 100;
 }
