e7e9d740b5e54db433ae5a629a8a7bec930b67f0
Vimmy <me@vimarshraina.com>
2017-03-01 11:42:26 +0000

    Android Fix for: MPLY-8160. Using the correct axis mappings wrt rotation of the device.Buddy: Jonty

--- android/src/com/eegeo/location/HeadingService.java
+++ android/src/com/eegeo/location/HeadingService.java
@@ -207,16 +207,16 @@ public class HeadingService implements SensorEventListener
         switch (rotation)
         {
         case Surface.ROTATION_0:
-        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_X, SensorManager.AXIS_Y, remap);
+        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Y, remap);
             break;
         case Surface.ROTATION_90:
         	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X, remap);
             break;
         case Surface.ROTATION_180:
-        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_Z, SensorManager.AXIS_X, remap);
+        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y, remap);
             break;
         default:
-        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, remap);
+        	SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X, remap);
             break;
         }
     }
