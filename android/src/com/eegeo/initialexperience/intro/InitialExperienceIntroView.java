0f7d16a11f227fbf16a563d7a8c71cb7a7a330c6
MichaelChanEegeo <michael.chan@kimbleoperations.com>
2017-04-11 10:57:39 +0100

    New welcome banner for android. Buddy: Vimmy

--- android/src/com/eegeo/initialexperience/intro/InitialExperienceIntroView.java
+++ android/src/com/eegeo/initialexperience/intro/InitialExperienceIntroView.java
@@ -6,6 +6,9 @@ import com.eegeo.entrypointinfrastructure.MainActivity;
 import com.eegeo.mobileexampleapp.R;
 
 import android.content.res.Resources;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
@@ -26,11 +29,14 @@ public class InitialExperienceIntroView implements View.OnClickListener, Animati
 	private int m_animationDuration;
 	
 	private RelativeLayout m_banner;
+	private RelativeLayout m_bannerInner;
 	private ViewGroup m_searchMenuDialog;
 	private ViewGroup m_compassDialog;
 	private ViewGroup m_mapmodeDialog;
 	private ViewGroup m_pinCreationDialog;
 	private View m_view;
+	private TextView m_welcomeIntroText;
+	private TextView m_welcomeIntroDescription;
 	
 	private Animation m_mainAnimationOn;
 	private Animation m_mainAnimationOff;
@@ -53,10 +59,13 @@ public class InitialExperienceIntroView implements View.OnClickListener, Animati
         m_view = m_activity.getLayoutInflater().inflate(R.layout.initial_experience_intro_layout, m_uiRoot, false);
         
         m_banner = (RelativeLayout)m_view.findViewById(R.id.initial_ux_intro_banner);
+		m_bannerInner = (RelativeLayout)m_view.findViewById(R.id.welcome_intro_banner_inner);
         m_searchMenuDialog = (ViewGroup)m_view.findViewById(R.id.initial_ux_search_menu_dialog);
         m_compassDialog = (ViewGroup)m_view.findViewById(R.id.initial_ux_compass_dialog);
         m_mapmodeDialog = (ViewGroup)m_view.findViewById(R.id.initial_ux_mapmode_dialog);
         m_pinCreationDialog = (ViewGroup)m_view.findViewById(R.id.initial_ux_pin_creation_dialog);
+		m_welcomeIntroText = (TextView)m_view.findViewById(R.id.welcome_intro_text);
+		m_welcomeIntroDescription = (TextView)m_view.findViewById(R.id.welcome_intro_description);
         
         Resources resources = m_activity.getResources();
         setDialogText(m_searchMenuDialog,
@@ -84,9 +93,46 @@ public class InitialExperienceIntroView implements View.OnClickListener, Animati
     	m_view.setOnClickListener(null);
 	    m_uiRoot.removeView(m_view);
 	}
+
+	public void scaleDimensions()
+	{
+		boolean isTablet = !m_activity.getResources().getBoolean(R.bool.isPhone);
+		if(isTablet)
+		{
+			DisplayMetrics metrics = m_activity.getApplicationContext().getResources().getDisplayMetrics();
+			Point screenSize = new Point();
+			m_activity.getWindowManager().getDefaultDisplay().getRealSize(screenSize);
+			final float dpScale = 1.7f / metrics.density;
+			final float tabletScalingFactor = (1705.f / screenSize.y) / dpScale;
+			final int introTextYPosition = (int) ((m_banner.getLayoutParams().height * 0.1f) / tabletScalingFactor);
+			final int descriptionTextYPosition = (int) ((m_banner.getLayoutParams().height * 0.55f) / tabletScalingFactor);
+			final float welcomeDescriptionTextsize = m_welcomeIntroDescription.getTextSize() / metrics.density;
+			final float welcomeIntroTextSize = m_welcomeIntroText.getTextSize() / metrics.density;
+
+			RelativeLayout.LayoutParams welcomeDescriptionLayout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			welcomeDescriptionLayout.setMargins(0, descriptionTextYPosition, 0, 0);
+			welcomeDescriptionLayout.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			m_welcomeIntroDescription.setLayoutParams(welcomeDescriptionLayout);
+			m_welcomeIntroDescription.setTextSize(TypedValue.COMPLEX_UNIT_SP, welcomeDescriptionTextsize / tabletScalingFactor);
+
+			RelativeLayout.LayoutParams introTextLayout = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+			introTextLayout.setMargins(0, descriptionTextYPosition, 0, 0);
+			introTextLayout.addRule(RelativeLayout.CENTER_HORIZONTAL);
+			introTextLayout.setMargins(0, introTextYPosition, 0, 0);
+			m_welcomeIntroText.setLayoutParams(introTextLayout);
+			m_welcomeIntroText.setTextSize(TypedValue.COMPLEX_UNIT_SP, welcomeIntroTextSize / tabletScalingFactor);
+
+			m_banner.getLayoutParams().height = (int) (m_banner.getLayoutParams().height / tabletScalingFactor);
+			m_banner.requestLayout();
+			m_bannerInner.getLayoutParams().height = (int) (m_bannerInner.getLayoutParams().height / tabletScalingFactor);
+			m_bannerInner.requestLayout();
+		}
+	}
     
     public void show()
     {
+		scaleDimensions();
+
     	m_awaitingInput = true;
     	m_view.setVisibility(View.VISIBLE);
 
@@ -103,6 +149,7 @@ public class InitialExperienceIntroView implements View.OnClickListener, Animati
     	m_compassDialog.startAnimation(compassAnim);
     	Animation pinCreateAnim = createDialogAnimation(0.0f, 1.0f, m_animationDuration/4, (m_animationDuration/4)*4);
     	m_pinCreationDialog.startAnimation(pinCreateAnim);
+		m_banner.bringToFront();
     }
 
     public void dismiss()
