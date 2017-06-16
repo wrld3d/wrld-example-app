00b66eede6510daadd9837ba6684d2aace0b33b7
blairmwrld <blair.mcsorley@kimbleoperations.com>
2017-06-16 08:51:19 +0100

    Adding "Show me the closest..." label and drop down separator to LHS menu, Windows. Buddy: Judith
    Fixed gaps that would sometimes appear after doing a search
    Increased height of dark blue separators to be consistent with mobile

---
+++ windows/ExampleApp/ExampleAppWPF/Views/SearchMenu/SearchMenuListAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace ExampleAppWPF
+{
+    class SearchMenuListAdapter : MenuListAdapter
+    {
+        private int m_indexOfFirstDropDown;
+
+        private double m_dropDownSeparatorHeight;
+
+        public SearchMenuListAdapter(bool shouldAlignIconRight,
+                                        ListBox list,
+                                        Storyboard slideInAnimation,
+                                        Storyboard slideOutAnimation,
+                                        Storyboard itemShutterOpenAnimation,
+                                        Storyboard itemShutterCloseAnimation,
+                                        string controlToAnimate,
+                                        bool isInKioskMode,
+                                        int indexOfFirstDropDown,
+                                        double separatorHeight)
+                                    : base(shouldAlignIconRight,
+                                          list,
+                                          slideInAnimation,
+                                          slideOutAnimation,
+                                          itemShutterOpenAnimation,
+                                          itemShutterCloseAnimation,
+                                          controlToAnimate,
+                                          isInKioskMode)
+        {
+            m_indexOfFirstDropDown = indexOfFirstDropDown - 1;
+
+            m_dropDownSeparatorHeight = separatorHeight;
+        }
+
+        protected override void NotifyDataSetChanged()
+        {
+            base.NotifyDataSetChanged();
+
+            for (int i = 0; i < Children.Count; ++i)
+            {
+                MenuListItem menuItem = Children[i];
+                if (menuItem is MenuListItem)
+                {
+                    Thickness margin = menuItem.ListItemMargin;
+                    margin.Top = 0;
+                    if (i == m_indexOfFirstDropDown)
+                    {
+                        menuItem.HasCustomSeparator = true;
+                    }
+                    else if (i - 1 == m_indexOfFirstDropDown)
+                    {
+                        margin.Top = m_dropDownSeparatorHeight;
+                    }
+                    menuItem.ListItemMargin = margin;
+                }
+            }
+        }
+    }
+}
