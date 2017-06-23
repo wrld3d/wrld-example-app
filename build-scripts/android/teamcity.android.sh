57a6e4c98c7144caa181dde1d907230099413ccc
Tim Jenks <tim.jenks@eegeo.com>
2017-06-23 10:03:50 +0100

    WIP production android configs. Buddy: MichaelC

--- build-scripts/android/teamcity.android.sh
+++ build-scripts/android/teamcity.android.sh
@@ -8,6 +8,25 @@ pathToReleaseKey=${5:-./release-key.keystore}
 hockeyAppIdentifier=${6:-62a47548d62a577300d5949f3f858683}
 shouldKeepLibs=${7:-1}
 apiVersion=$8
+environment=${9:staging}
+
+if [[ ( $environment != 'production' ) && ( $environment != 'staging' ) ]]; then
+  echo "invalid environment '$environment'. Must be one of [staging|production]"
+  exit 1
+fi
+
+apiKeyFile=./src/ApiKey.h
+apiKeyFileTemp=./src/ApiKeyTemp.h
+git checkout $apiKeyFile
+
+if [ $environment == 'production' ]; then
+  sed -e "s/project_swallow_config.json/project_swallow_production_config.json/g" $apiKeyFile > $apiKeyFileTemp
+  if [ $? -ne 0 ] ; then
+    echo "Failed to poke config file value into ApiKey file"  >&2
+    exit 1
+  fi
+  mv $apiKeyFileTemp $apiKeyFile
+fi
 
 #poke the version in the manifest
 manifest=$pathToProjectDir/AndroidManifest.xml
