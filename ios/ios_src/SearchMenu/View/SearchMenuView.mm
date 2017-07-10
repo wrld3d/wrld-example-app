9a9d2f455cd70f2fea46be0c19d7fe0118a257b4
blairmeegeo <blair.mcsorley@kimbleoperations.com>
2017-07-10 09:20:53 +0100

    Fix for MPLY-8590 and MPLY-8542, Android, iOS and Windows. Buddy: Malcolm
    Previous fix for MPLY-8542 caused MPLY-8590 so reverted that fix and
    cleared the search result list on Windows in SearchMenuView.cs when
    search result count == 0.
    On Android and iOS, although the search results cleared, the small
    white arrow stayed on screen, so now hiding that when no search results.

--- ios/ios_src/SearchMenu/View/SearchMenuView.mm
+++ ios/ios_src/SearchMenu/View/SearchMenuView.mm
@@ -703,8 +703,6 @@
     
     if(!m_resultsVisible)
     {
-        m_pAnchorAnimationController->Play();
-        
         if([super isAnimating])
         {
             m_titleContainersRequireRefresh = true;
@@ -727,6 +725,15 @@
         m_resultsVisible = true;
     }
     
+    if(searchResultCount > 0)
+    {
+        m_pAnchorAnimationController->Play();
+    }
+    else if(self.pAnchorArrowImage.frame.size.height > 0)
+    {
+        m_pAnchorAnimationController->PlayReverse();
+    }
+    
     [self.pInputDelegate setHasResults: searchResultCount>0];
 }
 
@@ -736,7 +743,10 @@
     
     if(m_resultsVisible)
     {
-        m_pAnchorAnimationController->PlayReverse();
+        if(self.pAnchorArrowImage.frame.size.height > 0)
+        {
+            m_pAnchorAnimationController->PlayReverse();
+        }
         
         if([super isAnimating])
         {
