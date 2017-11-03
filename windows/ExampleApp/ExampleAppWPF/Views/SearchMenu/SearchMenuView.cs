0c747d0722bd90082a5ce8931c6144c28ed69b66
Tom Paynter <tom.paynter@dundee.kimbleoperations.com>
2017-11-03 15:45:32 +0000

    Revert "MPLY-9040 fix, The Search Below The Fold button doesn't steal input on Kiosk. Buddy: John"
    This reverts commit 23138942dd0c870fc3ce6264fcc9af2c28a95441.

--- windows/ExampleApp/ExampleAppWPF/Views/SearchMenu/SearchMenuView.cs
+++ windows/ExampleApp/ExampleAppWPF/Views/SearchMenu/SearchMenuView.cs
@@ -203,8 +203,6 @@ namespace ExampleAppWPF
             m_searchResultsScrollButton = (RepeatButton)GetTemplateChild("SearchResultsScrollButton");
             m_searchResultsScrollButton.Click += OnResultsScrollButtonMouseDown;
             m_searchResultsScrollButton.PreviewMouseWheel += OnResultsMenuScrollWheel;
-            m_searchResultsScrollButton.MouseUp += OnResultsScrollButtonMouseUp;
-            m_searchResultsScrollButton.MouseLeave += OnResultsScrollButtonMouseLeave;
 
             m_resultsClearButton = (Button)GetTemplateChild("SearchClear");
             m_resultsClearButton.Click += OnResultsClear;
@@ -322,18 +320,7 @@ namespace ExampleAppWPF
 
         private void OnResultsScrollButtonMouseDown(object sender, RoutedEventArgs e)
         {
-            m_resultsListScrollViewer.IsEnabled = false;
             m_resultsListScrollViewer.ScrollToVerticalOffset(m_resultsListScrollViewer.VerticalOffset + m_scrollSpeed);
-        }
-
-        private void OnResultsScrollButtonMouseUp(object sender, RoutedEventArgs e)
-        {
-            m_resultsListScrollViewer.IsEnabled = true;
-        }
-
-        private void OnResultsScrollButtonMouseLeave(object sender, RoutedEventArgs e)
-        {
-            m_resultsListScrollViewer.IsEnabled = true;
         }
 
         private void OnResultsListBoundaryFeedback(object sender, ManipulationBoundaryFeedbackEventArgs e)
