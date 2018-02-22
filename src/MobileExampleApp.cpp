abebccf39ae67ca5af9a70703d9fe7c770384c48
Jordan Brown <jordan.brown@wrld3d.com>
2018-02-22 16:35:44 +0000

    MPLY-9283
    Fix issue with camera controls not working on first run of the app
    
    Cherrypick b6deb29 - for [MPLY-9283]; Now dispatching the InitialExperienceIntroDismissedMessage() to complete the non-existant intro tutorial, if in kiosk mode. Buddy: Andrew G.

--- src/MobileExampleApp.cpp
+++ src/MobileExampleApp.cpp
@@ -1200,6 +1200,10 @@ namespace ExampleApp
             InitialExperience::SdkModel::IInitialExperienceModel& initialExperienceModel = m_initialExperienceModule.GetInitialExperienceModel();
             if(!initialExperienceModel.HasCompletedInitialExperience() && IsLoadingScreenComplete())
             {
+				if (m_applicationConfiguration.IsInKioskMode())
+				{
+					m_messageBus.Publish(ExampleApp::InitialExperience::InitialExperienceIntroDismissedMessage());
+				}
                 InitialExperience::SdkModel::IInitialExperienceController& initialExperienceController = m_initialExperienceModule.GetInitialExperienceController();
                 initialExperienceController.Update(dt);
             }
