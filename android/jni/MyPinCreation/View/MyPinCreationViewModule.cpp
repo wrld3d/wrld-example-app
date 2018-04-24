69f81d22fafa385be3e00c1b09e20eb86da76b5e
Salvador Girbau <salvador.girbau@wrld3d.com>
2018-04-24 16:05:37 +0100

    Widget integration (android): removed pin creation button
    Getting sure that the "Drop pin" menu item (where the same
    functionality is called from) still works.
    
    Buddy: Tom Paynter

--- android/jni/MyPinCreation/View/MyPinCreationViewModule.cpp
+++ android/jni/MyPinCreation/View/MyPinCreationViewModule.cpp
@@ -1,8 +1,6 @@
 // Copyright eeGeo Ltd (2012-2015), All Rights Reserved
 
 #include "MyPinCreationViewModule.h"
-#include "MyPinCreationInitiationView.h"
-#include "MyPinCreationInitiationController.h"
 #include "MyPinCreationConfirmationView.h"
 #include "MyPinCreationConfirmationController.h"
 
@@ -21,9 +19,6 @@ namespace ExampleApp
                 Metrics::IMetricsService& metricsService
             )
             {
-                m_pInitiationView = Eegeo_NEW(MyPinCreationInitiationView)(nativeState);
-                m_pInitiationController = Eegeo_NEW(MyPinCreationInitiationController)(initiationViewModel, *m_pInitiationView, confirmationViewModel, messageBus, metricsService);
-
                 m_pConfirmationView = Eegeo_NEW(MyPinCreationConfirmationView)(nativeState);
                 m_pConfirmationController = Eegeo_NEW(MyPinCreationConfirmationController)(confirmationViewModel, *m_pConfirmationView, detailsViewModel, messageBus, metricsService);
             }
@@ -32,9 +27,6 @@ namespace ExampleApp
             {
                 Eegeo_DELETE m_pConfirmationController;
                 Eegeo_DELETE m_pConfirmationView;
-
-                Eegeo_DELETE m_pInitiationController;
-                Eegeo_DELETE m_pInitiationView;
             }
         }
     }
--- android/jni/MyPinCreation/View/MyPinCreationViewModule.h
+++ android/jni/MyPinCreation/View/MyPinCreationViewModule.h
@@ -20,9 +20,6 @@ namespace ExampleApp
             class MyPinCreationViewModule : public IMyPinCreationViewModule, private Eegeo::NonCopyable
             {
             private:
-                MyPinCreationInitiationView* m_pInitiationView;
-                MyPinCreationInitiationController* m_pInitiationController;
-
                 MyPinCreationConfirmationView* m_pConfirmationView;
                 MyPinCreationConfirmationController* m_pConfirmationController;
 
