61f0bcdeee33f9697d148fc6e06f38224f636f48
Tim Jenks <tim.jenks@eegeo.com>
2018-05-17 10:52:02 -0400

    No longer pass a constant versionCode when generating build. Buddy: None

--- build-scripts/android/teamcity.android.sh
+++ build-scripts/android/teamcity.android.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-versionName=$1
-buildUrl=$2
-githubUrl=$3
-pathToProjectDir=${4}
-hockeyAppIdentifier=${5:-62a47548d62a577300d5949f3f858683}
-environment=${6:staging}
-config_password=$7
+versionCode=$1
+versionName=$2
+buildUrl=$3
+githubUrl=$4
+pathToProjectDir=${5}
+hockeyAppIdentifier=${6:-62a47548d62a577300d5949f3f858683}
+environment=${7:staging}
+config_password=$8
 
 if [[ ( $environment != 'staging' ) && ( $environment != 'production' ) && ( $environment != 'release' ) ]]; then
   echo "invalid environment '$environment'. Must be one of [staging|production|release]"
@@ -15,7 +16,7 @@ fi
 
 sh build-scripts/encrypt_config.sh -p android -e $environment -j $config_password
 
-sh "./build-scripts/android/create_apk_file_gradle.step.sh" $pathToProjectDir  1 $versionName 
+sh "./build-scripts/android/create_apk_file_gradle.step.sh" $pathToProjectDir  $versionCode $versionName 
 if [ $? -ne 0 ] ; then
   exit 1
 fi
