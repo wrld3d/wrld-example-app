4a9ab8a58c0b1d25128bd66dfd7f0188190ba55b
SamAins <sam.ainsworth@wrld3d.com>
2018-06-07 14:46:30 +0100

    Implmeneted labels changing for floors id's when the indoor map model is loaded on iOS.
    Buddy/Pair: Ali A

--- ios/ios_src/NavRouting/View/NavWidgetView.h
+++ ios/ios_src/NavRouting/View/NavWidgetView.h
@@ -66,7 +66,7 @@ namespace ExampleApp
                 void ClearStartLocation() ;
                 void ClearEndLocation() ;
                 
-                void SetRoute(const SdkModel::NavRoutingRouteModel& routeModel);
+                void SetRoute(const SdkModel::NavRoutingRouteModel& routeModel,  bool isNewRoute);
                 
                 void ClearRoute();
                 
