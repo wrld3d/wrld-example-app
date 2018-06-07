b500a7a8b00e69ae4ef2a58151a5f35515f9ab3c
Malcolm Brown <DragonXVIFB@GMail.com>
2018-06-07 12:17:34 +0100

    Fixes to camera + step selection bugs. Buddy: Ali
    - Fixed issue where resizing route size caused View to reclamp selected page and act as a selection, causing an oscillation of index setting between threads when assigning a new route. - indices are now set to 0 before assigning route.
    - Same for starting Turn By Turn
    - Disabled case where we automatically enabled compass if you selected the current step as your selected step - you now have to manually engage the compass to do this
    - Disabled any transitions when starting turn by turn, as this was fighting with the compass
    - Other logic fixups in NavRoutingCameraController

--- src/NavRouting/SdkModel/NavRoutingCameraController.h
+++ src/NavRouting/SdkModel/NavRoutingCameraController.h
@@ -30,7 +30,6 @@ namespace ExampleApp
                 
                 void OnRouteSet(const NavRoutingRouteModel& routeModel);
                 void OnSelectedDirectionSet(const int& selectedDirection);
-                void OnCurrentDirectionSet(const int& currentDirection);
                 void OnNavModeSet(const NavRoutingMode& navMode);
                 void OnGpsModeChanged();
                 
@@ -39,7 +38,6 @@ namespace ExampleApp
                 
                 Eegeo::Helpers::TCallback1<NavRoutingCameraController, const NavRoutingRouteModel&> m_routeSetCallback;
                 Eegeo::Helpers::TCallback1<NavRoutingCameraController, const int> m_selectedDirectionSetCallback;
-                Eegeo::Helpers::TCallback1<NavRoutingCameraController, const int> m_currentDirectionSetCallback;
                 Eegeo::Helpers::TCallback1<NavRoutingCameraController, const NavRoutingMode> m_navModeSetCallback;
                 Eegeo::Helpers::TCallback0<NavRoutingCameraController> m_gpsModeChangedCallback;
                 
