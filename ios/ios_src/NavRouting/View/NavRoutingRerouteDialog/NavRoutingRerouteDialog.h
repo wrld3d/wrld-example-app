ad977da126c93d8414a1696b5cbc02284b962fe0
Ali Arslan <ali.arslan@wrld3d.com>
2018-06-13 10:25:47 +0100

    Added reroute dialog on iOS. Buddy: michael.odonnell

---
+++ ios/ios_src/NavRouting/View/NavRoutingRerouteDialog/NavRoutingRerouteDialog.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+#include "ICallback.h"
+
+@interface NavRoutingRerouteDialog : UIView
+
+@property(nonatomic, copy) IBOutlet NSString* message;
+
+- (void) InsertRerouteDialogClosedCallback:(Eegeo::Helpers::ICallback1<const bool>*) rerouteDialogClosedCallback;
+- (void) RemoveRerouteDialogClosedCallback:(Eegeo::Helpers::ICallback1<const bool>*) rerouteDialogClosedCallback;
+@end
